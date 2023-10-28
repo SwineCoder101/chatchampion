96567be2e65113c169700faa6c0a67d9d669b5eb
Liam Faruq <liam@Liams-MacBook-Pro.local>
2023-10-28 19:31:06 +0100

    update-build

--- telegram-service/dist/bot.js
+++ telegram-service/dist/bot.js
@@ -15,10 +15,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
+const ChatCache_1 = __importDefault(require("./ChatCache"));
+const flock_main_1 = __importDefault(require("./flock-main"));
 dotenv_1.default.config();
 const { PORT, TELEGRAM_TOKEN, SERVER_URL } = process.env;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const updateArray = [];
 app.get("/", (req, res) => {
     res.send("Welcome to the bot server!");
 });
@@ -34,13 +37,134 @@ const setupWebhook = () => __awaiter(void 0, void 0, void 0, function* () {
         console.error("Error setting up the webhook:", error.message);
     }
 });
-app.post(URI, (req, res) => {
-    console.log(req.body);
-    res.status(200).send('ok');
+const deleteWebhook = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { data } = yield axios_1.default.get(`${TELEGRAM_API}/deleteWebhook`);
+        console.log(data);
+    }
+    catch (error) {
+        console.error("Error deleting the webhook:", error.message);
+    }
 });
+const getUpdates = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { data } = yield axios_1.default.get(`${TELEGRAM_API}/getUpdates`);
+        return data;
+    }
+    catch (error) {
+        console.error("Error getting updates:", error.message);
+    }
+});
+const getGroupUpdates = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { data } = yield axios_1.default.get(`${TELEGRAM_API}/getUpdates`);
+        const filteredMessages = filterMessagesByChannelTitle(data, "Chat Championship");
+        return filteredMessages;
+    }
+    catch (error) {
+        console.error("Error getting updates:", error.message);
+    }
+});
+function filterMessagesByChannelTitle(messages, channelTitle) {
+    return messages.result.filter(update => update.message.chat.title === channelTitle);
+}
+function filterGroupChannels(messages) {
+    return messages.result.filter(update => update.message.chat.type === 'group');
+}
+function filterMessages(messages, options) {
+    return messages.result.filter(update => {
+        const message = update.message;
+        const from = message.from;
+        // Check if the message is from the specified user name
+        const isUserNameMatch = options.userName ? from.username === options.userName : true;
+        // Check if the message is from the specified user ID
+        const isUserIdMatch = options.userId ? from.id === options.userId : true;
+        // Check if the message is within the specified time range
+        const isWithinTimeRange = options.startTime && options.endTime
+            ? message.date >= options.startTime && message.date <= options.endTime
+            : true;
+        return isUserNameMatch && isUserIdMatch && isWithinTimeRange;
+    });
+}
+// Example usage:
+const options = {
+    userName: "LiamTel",
+    userId: 5338078853,
+    startTime: 1698452054,
+    endTime: 1698452093 // End time (Unix timestamp)
+};
+const sendMessageCondition = (sentMessage, condition, sendMessage) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        if (sentMessage === condition) {
+            const { data } = yield axios_1.default.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: 123456789,
+                text: sendMessage
+            });
+            console.log(data);
+        }
+    }
+    catch (error) {
+        console.error("Error sending message:", error.message);
+    }
+});
+const sendMessage = (chatId, sendMessage) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { data } = yield axios_1.default.post(`${TELEGRAM_API}/sendMessage`, {
+            chat_id: chatId,
+            text: sendMessage
+        });
+    }
+    catch (error) {
+        console.error("Error sending message:", error.message);
+    }
+});
+const resetUpdates = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield setupWebhook();
+    yield deleteWebhook();
+});
+app.post(URI, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const chunk = req.body;
+        const chatId = chunk.message.chat.id;
+        const userId = chunk.message.from.id;
+        const sentMessage = chunk.message.text;
+        console.log('chatId', chatId);
+        console.dir(req.body);
+        updateArray.push(req.body);
+        ChatCache_1.default.addUpdate(chatId, req.body);
+        //create a wallet
+        if (sentMessage === '/createWallet') {
+            sendMessage(chatId, 'about to create wallet here');
+        }
+        //analyze the chat
+        if (sentMessage === '/analyze') {
+            const updates = ChatCache_1.default.getUpdates(chatId);
+            sendMessage(chatId, 'Analysing the chat now ...');
+            const analysis = yield (0, flock_main_1.default)(updates);
+            console.log(analysis);
+            sendMessage(chatId, analysis);
+            ChatCache_1.default.resetChat(chatId);
+        }
+        // trial test the chat
+        if (sentMessage === '/showchat') {
+            const updates = ChatCache_1.default.getUpdates(chatId);
+            console.log("=========UPDATES==========");
+            console.log(updates);
+            console.log("=========UPDATES==========");
+        }
+        // Send the response after the asynchronous operation is complete
+        res.status(200).send('ok');
+    }
+    catch (error) {
+        console.error(error);
+        // Since no response has been sent yet, it's safe to send the error response here
+        res.status(500).send('Internal Server Error');
+    }
+}));
 app.get(URI, (req, res) => {
     res.send('Hello');
 });
+let offset = 0;
 app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log(`Server running on port ${PORT}`);
     console.log(`Webhook URL: ${webhookURL}`);
--- telegram-service/dist/flock-main.js
+++ telegram-service/dist/flock-main.js
@@ -1,1 +1,135 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const openai_1 = __importDefault(require("openai"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config({ path: "/.env" });
+const ethers_1 = require("ethers");
+const openai = new openai_1.default({
+    apiKey: "",
+    baseURL: "http://flock.tools:8001/v1", // defaults to https://api.openai.com/v1
+});
+const instruction = `can you rate the participants in this conversation based on their humor? Take 1000 points and distrbute them amongst each user based on their level of humor and map it against their name. The format is a json array of username: score  Only write the json data and not any text. At the end of the analysis give a json summary`;
+const instruction1 = `can you rate the participants in this conversation based on their humor? Take 1000 points and distrbute them amongst each user based on their level of humor and map it against their name. The format is a json array of username: score  Only write the json data and not any text.`;
+const instruction2 = `In the chatroom below, participants were rated based on the humor level of their messages. Using the provided leaderboard and messages, determine the reasoning behind each participant's score. How do their messages reflect their ranking on the leaderboard? Consider elements such as wit, puns, timing, and the context of the conversation.`;
+const tokenAbi = [
+    "function mint(address to, uint256 amount) public"
+];
+function getMessages() {
+    return `{"ok":true,"result":[{"update_id":177312581,
+  "message":{"message_id":23,"from":{"id":5338078853,"is_bot":false,"first_name":"Liam","username":"LiamTel","language_code":"en"},"chat":{"id":-4066054986,"title":"Chat Championship","type":"group","all_members_are_administrators":true},"date":1698452054,"text":"hey henrik whatsup , this is a test"}},{"update_id":177312582,
+  "message":{"message_id":24,"from":{"id":1386759162,"is_bot":false,"first_name":"Henrik","username":"henrik1111","language_code":"en"},"chat":{"id":-4066054986,"title":"Chat Championship","type":"group","all_members_are_administrators":true},"date":1698452068,"text":"Thanks for giving sample text"}},{"update_id":177312583,
+  "message":{"message_id":25,"from":{"id":1386759162,"is_bot":false,"first_name":"Henrik","username":"henrik1111","language_code":"en"},"chat":{"id":-4066054986,"title":"Chat Championship","type":"group","all_members_are_administrators":true},"date":1698452071,"text":"lol"}},{"update_id":177312584,
+  "message":{"message_id":26,"from":{"id":5338078853,"is_bot":false,"first_name":"Liam","username":"LiamTel","language_code":"en"},"chat":{"id":-4066054986,"title":"Chat Championship","type":"group","all_members_are_administrators":true},"date":1698452083,"text":"do you know whats blue and not heavy?"}},{"update_id":177312585,
+  "message":{"message_id":27,"from":{"id":1386759162,"is_bot":false,"first_name":"Henrik","username":"henrik1111","language_code":"en"},"chat":{"id":-4066054986,"title":"Chat Championship","type":"group","all_members_are_administrators":true},"date":1698452088,"text":"no"}},{"update_id":177312586,
+  "message":{"message_id":28,"from":{"id":5338078853,"is_bot":false,"first_name":"Liam","username":"LiamTel","language_code":"en"},"chat":{"id":-4066054986,"title":"Chat Championship","type":"group","all_members_are_administrators":true},"date":1698452093,"text":"light blue"}},{"update_id":177312587,
+  "message":{"message_id":29,"from":{"id":1386759162,"is_bot":false,"first_name":"Henrik","username":"henrik1111","language_code":"en"},"chat":{"id":-4066054986,"title":"Chat Championship","type":"group","all_members_are_administrators":true},"date":1698452097,"text":"ahhahhah'"}}]}`;
+}
+function formatMessages(jsonData) {
+    const data = JSON.parse(jsonData);
+    // Check if the structure of the JSON is as expected
+    if (!data.ok || !data.result || !Array.isArray(data.result)) {
+        throw new Error("Invalid JSON format");
+    }
+    const messages = data.result.map((item) => {
+        const username = item.message.from.username || item.message.from.first_name;
+        const messageText = item.message.text;
+        return `${username}: ${messageText}`;
+    });
+    return messages.join("\n");
+}
+function analyzeChat(prompt) {
+    var _a;
+    return __awaiter(this, void 0, void 0, function* () {
+        const currentDateTime = new Date().toLocaleString();
+        //console.log(`Routine executed at: ${currentDateTime}`);
+        //console.log("Prompt:", prompt);
+        //console.log("Model:", process.env.model_name);
+        try {
+            const chatCompletion = yield openai.chat.completions.create({
+                messages: [{ role: "user", content: prompt }],
+                model: "hackathon-chat",
+            });
+            //console.log("Response:", chatCompletion);
+            //console.log("Choices:", chatCompletion?.choices[0].message);
+            return (_a = chatCompletion === null || chatCompletion === void 0 ? void 0 : chatCompletion.choices[0]) === null || _a === void 0 ? void 0 : _a.message.content;
+        }
+        catch (error) {
+            console.error("Error getting completion from OpenAI:", error);
+            throw error;
+        }
+    });
+}
+function mintTokens(providerUrl, privateKey, contractAddress, mintInfos) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const provider = new ethers_1.ethers.JsonRpcProvider(providerUrl);
+        const wallet = new ethers_1.ethers.Wallet(privateKey, provider);
+        const contract = new ethers_1.ethers.Contract(contractAddress, tokenAbi, wallet);
+        for (let info of mintInfos) {
+            try {
+                const tx = yield contract.mint(info.address, info.amount);
+                yield tx.wait(); // Wait for the transaction to be mined
+                console.log(`Minted ${info.amount} tokens for address: ${info.address}`);
+            }
+            catch (err) {
+                console.error(`Error minting tokens for address: ${info.address}. Error: ${err.message}`);
+            }
+        }
+    });
+}
+function startAnalysis(messages) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("Result:\n");
+        var result = yield analyzeChat(instruction + messages);
+        console.log(result);
+        console.log("Reasoning:\n");
+        var reasoning = yield analyzeChat(instruction2 + "\n\nLeaderboard:\n" + result + "\n\nChatroom messages:\n" + messages);
+        return reasoning;
+    });
+}
+// async function main() {
+//   var messages = formatMessages(getMessages());
+//   console.log("Result:\n");
+//   var result = await analyzeChat(instruction + messages);
+//   console.log(result);
+//   console.log("Reasoning:\n");
+//   var reasoning = await analyzeChat(instruction2 + "\n\nLeaderboard:\n" + result + "\n\nChatroom messages:\n" + messages);
+//   console.log(reasoning);
+// }
+// main();
+exports.default = startAnalysis;
 //# sourceMappingURL=flock-main.js.map
